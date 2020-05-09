55dcf747de287b4b4c6ae4f6dbf7f296775ce202
oleoot <oleoot28@gmail.com>
2020-05-09 17:44:14 +0300

    Add post works

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ import Profile from './components/Profile';
 function App() {
   const [friends, addFriend] = useState(false)
 
-  // function Addfriend(friends) {
-  //   addFriend(!friends)
-  // }
 
 
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,43 +4,128 @@ import React, { useState } from 'react';
 
 
 
+function Post({ post, index }) {
+    return (
+        <div className="post-wrap">
+            <div>
+                <img src={require(`../img/avatar-grey-cropped.jpg`)} alt="" />
+            </div>
+            <div className="post-text">
+                <p className="post-text">{post.text}</p>
+            </div>
+        </div>
+
+    )
+}
+
+
+
+
 
 function Profile(props) {
+    const [posts, setPost] = useState([])
+    const [clickPost, openPostPane] = useState(false);
 
     function AddFriend(friends) {
         props.updateFriend(!friends)
     }
-    console.log(props.friends)
+
+    function PostForm({ addPost }) {
+        const [value, setValue] = useState();
+
+        const handleSubmit = e => {
+            e.preventDefault();
+            if (!value) return;
+            addPost(value);
+            setValue('');
+        }
+        return (
+            <form onSubmit={handleSubmit}>
+                <input className="add-post" type="text" value={value} placeholder={"Дoбавить пост..."} onChange={(e) => { setValue(e.target.value) }} />
+
+            </form>
+        )
+
+
+    }
+    const addPost = text => {
+        const newPost = [...posts, { text }];
+        setPost(newPost)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div className="container">
             <div className="profile">
                 <div className="profile-about">
                     <div>
                         <img src={require(`../img/avatar-grey-cropped.jpg`)} alt="" />
-                        {props.friends === false ?
-                            <button onClick={() => { AddFriend(props.friends) }}>Добавить в друзья</button> :
-                            <button onClick={() => { AddFriend(props.friends) }}>Заявка отправлена</button>
-                        }
                     </div>
                     <div>
-                        <p>Name</p>
-                        <p>City</p>
-                        <p>Uni</p>
+                        <p>Имя: Олег Куць</p>
+                        <p>Город: Киев</p>
+                        <p>Университет: КНЕУ</p>
                     </div>
                 </div>
                 <div className="profile-news">
-                    <ul>
-                        <li>Посты</li>
-                        <li>Друзья</li>
-                        <li>Сообщества</li>
-                    </ul>
-                    <div className="post-wrap">
-                        <input type="text" placeholder="написать пост" />
-                        <button>Запостить</button>
+                    <div>
+                        {props.friends === false ?
+                            <button onClick={() => { AddFriend(props.friends) }}>Добавить в друзья</button> :
+                            <button className="btn-pushed" onClick={() => { AddFriend(props.friends) }}>Заявка отправлена</button>
+                        }
                     </div>
+                    <div>
+                        <ul className="prof-ul">
+                            <div onClick={() => openPostPane(!clickPost)}>
+                                <p>Посты</p>
+                                {/* {clickPost === true ?
+                                    <div>
+                                        <PostForm addPost={addPost} />
+                                        {posts.map((post, index) => (
+                                            <Post key={index} index={index} post={post} />
+                                        ))}
+
+                                    </div> :
+                                    <div></div>
+                                } */}
+                            </div>
+                            <div><p>Друзья</p></div>
+                            <div><p>Сообщества</p></div>
+                        </ul>
+                    </div>
+
                 </div>
+
             </div>
-        </div>
+            <div className="panes-switch-wrap">
+                {clickPost === true ?
+                    <div>
+                        <PostForm addPost={addPost} />
+                        {posts.map((post, index) => (
+                            <Post key={index} index={index} post={post} />
+                        ))}
+
+                    </div> :
+                    <div></div>
+                }
+            </div>
+        </div >
     );
 }
 
